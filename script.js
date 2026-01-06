e22e3fd7e8be1be476511a66be5e1b6594e63984
Iury-AB <iuryalves.iab@gmail.com>
2026-01-06 15:14:59 -0300

    primeiros testes e config discord

--- script.js
+++ script.js
@@ -117,6 +117,10 @@ function salvarFicha() {
 }
 
 function carregarFicha(nome) {
+  if (nome == "selecionar") {
+    limparFicha();
+    return;
+  }
   const dados = JSON.parse(localStorage.getItem(`ficha:${nome}`));
   if (!dados) return;
 
@@ -700,13 +704,17 @@ document.addEventListener("click", function (event) {
 
   // todos os menus abertos (principal + submenus)
   const menusAbertos = document.querySelectorAll(".menu.show");
-  const botaoAjustes = document.getElementById("botao-ajustes");
+  const botoesMenu = document.querySelectorAll(".botao-menu");
 
   // se não há menus abertos, não faz nada
   if (menusAbertos.length === 0) return;
 
-  // clicou no botão de ajustes? não fecha
-  if (botaoAjustes && botaoAjustes.contains(event.target)) return;
+  // clicou em QUALQUER botão de menu? não fecha
+  for (const botao of botoesMenu) {
+    if (botao.contains(event.target)) {
+      return;
+    }
+  }
 
   // clicou dentro de QUALQUER menu aberto? não fecha
   for (const menu of menusAbertos) {
@@ -719,3 +727,79 @@ document.addEventListener("click", function (event) {
   menusAbertos.forEach(menu => menu.classList.remove("show"));
 });
 
+
+function salvarConfig () {
+  const dados = {};
+
+  document.querySelectorAll(".config").forEach(el => {
+    if (el.id){
+      if(el.type == "text"){
+        dados[el.id] = el.value;
+      }
+      else if(el.type="checkbox"){
+        dados[el.id] = el.checked;
+      }
+    }
+  });
+
+  localStorage.setItem(`config-ficha-hoffens`, JSON.stringify(dados));
+}
+
+function carregarConfig() {
+  const dados = JSON.parse(localStorage.getItem("config-ficha-hoffens"));
+  if (!dados) return;
+
+  Object.entries(dados).forEach(([id, value]) => {
+    const field = document.getElementById(id);
+
+    if (field.type == "text") {
+      field.value = value;
+    }
+    else if (field.type == "checkbox") {
+      field.checked = value;
+    }
+  });
+}
+
+document.addEventListener("input", function (e) {
+  if (e.target.classList.contains("config")) {
+    salvarConfig();
+  }
+});
+
+carregarConfig();
+
+function enviarRequest(dados, server, canal) {
+  
+  fetch(server+canal, {
+    method: "POST",
+    body: JSON.stringify(dados),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+    .then((response) => response.json())
+    .then(json => alert(JSON.stringify(json, null, 2)));
+
+}
+
+function testeAtributo(id, nome) {
+  var atributo = Number(document.getElementById(id).value) || 0;
+  var rolagem = Math.floor(Math.random() * 20) + 1;
+
+  var resultado = rolagem + atributo;
+
+  const jogador = document.getElementById("usuario-discord").value || "---";
+  const servidorDiscord = document.getElementById("servidor-discord").value;
+
+  var dados = {
+    [`resultado.${nome}`]: resultado,
+    [`Hab.${nome}`]: atributo,
+    "discordID": servidorDiscord,
+    "Jogador": jogador
+  };
+
+  const bot = document.getElementById("bot-discord").value;
+
+  enviarRequest(dados, bot, "/habilidade");
+}
